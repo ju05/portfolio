df0109359b01d870882f0269e025b0e3ad225059
Juliana <j.amaralwork@gmail.com>
2022-09-18 10:33:17 +0300

    responsive portfolio

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// Open mailto links in a new tab
+let hamburguer = document.getElementById('hamburguer');
+let navUL = document.getElementById('nav-ul');
+
+hamburguer.addEventListener('click', function() {
+    navUL.classList.toggle('show')
+})
+
+let coverOverlay = document.getElementById('cover-overlay');
+let hoverText = document.getElementById('hover-text')
+
+coverOverlay.addEventListener('hover', function(){
+    hoverText.style.display == 'block'
+})
+
 $('#mailto').click(function (e) {
     e.preventDefault();
     var url = $(this).attr('href');
